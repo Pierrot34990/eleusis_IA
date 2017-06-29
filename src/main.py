4461ab2a27389563942a5a24b0453670382e63db
Pierrot34990 <p.baumes@gmail.com>
2017-06-29 15:31:32 +0200

    Add rules game

--- src/main.py
+++ src/main.py
@@ -2,37 +2,67 @@
 # -*- coding:Utf8 -*-
 
 from webServiceFactory import WebServiceFactory
-import time
+import time, json
 
 class Main:
 
     idJoueur = 0
+    numJoueur = 0
+    CONST_DIEUX_INVENTE_UNE_REGLE = 0
+    CONST_DIEUX_VERIFIE_DES_CARTES = 1
+    CONST_DIEUX_DIT_SI_PROPHETE = 2
 
     def launch(self):
-
         endGame = False
         self.connect()
 
         while endGame == False:
             result = self.turn(self.idJoueur)
 
-            if result['commencerPartie'] == True:
-                print('C\'est à moi de jouer !!')
+            if result['commencerPartie'] == True and ('gameAlreadyStart' in result) and result['gameAlreadyStart'] == False:
+                self.numJoueur = self.ready(self.idJoueur)
             else:
-                print('C\'est pas à moi de jouer !!')
+                if result['finPartie'] == True:
+                    print("Fin de partie !!")
+                else:
+                    if result['status'] == 1:
+                        print ("C'est à moi de jouer")
+                        if result['numJoueur'] == 'god':
+                            print ("Jouer en tant que Dieu .")
+                            if result['godRole'] == self.CONST_DIEUX_INVENTE_UNE_REGLE:
+                                print ("Jouer en tant que Dieu, J'invente une règle.")
+                            elif result['godRole'] == self.CONST_DIEUX_DIT_SI_PROPHETE:
+                                print("Jouer en tant que Dieu, Je dit prophète.")
+                            elif result['godRole'] == self.CONST_DIEUX_VERIFIE_DES_CARTES:
+                                print("Jouer en tant que Dieu, Je vérifie les cartes.")
+                                print('CARTES TEST = ' + self.getCardsTest())
+                                #self.checkCards(self, result['partie']['selectedCards'])
+                        else:
+                            print ("Jouer en tant que joueur .")
+                    else:
+                        print("C'est pas à moi de jouer !!")
 
             time.sleep(1)
 
     def connect(self):
-
         w = WebServiceFactory()
         self.idJoueur = w.webServiceConnect()
 
     def turn(self, idPlayer):
-
         w = WebServiceFactory()
         response = w.webServiceTurn(idPlayer)
         return response
+
+    def ready(self, idPlayer):
+        w = WebServiceFactory()
+        response = w.webServiceReady(idPlayer)
+        return response
+
+    #def checkCards(self, cards):
+
+
+    def getCardsTest(self):
+        return json.dumps({'color': 'S','number': 2})
 
 if __name__ == '__main__':
     m = Main()
---
+++ src/test.py
@@ -0,0 +1,53 @@
+import json
+
+class Main:
+
+    numCard = [1,2,3,4,5,6,7,8,9,10,11,12,13]
+    peer = ['yes','no']
+    cardType = ['S', 'D', 'C', 'H']
+    colorPlayed = []
+    colorsChecked = 0
+    numberCardsToPut = [1,2,3,4,5]
+    redundancy = [0,1,2,3,4]  # The number of intermediate(s) card(s)
+    alternancy = ['yes', 'no']  # ex : One red card then one black card
+
+    def getCardsTest(self):
+        data = json.dumps({'cards':[{'color': 'S','number': 2}, {'color':'C','number':3}]})
+        return data
+
+    def getFakeRule(self):
+        data = {'numberCardsToPut':2,'color':'noir','numCard':None,'cardType':None,'redundancy':0, 'alternancy':'no'}
+        return data
+
+    # Check if rule is respected on cards played
+    def checkRules(self):
+        cardsPlayed = self.getCardsTest()
+        item_dict = json.loads(cardsPlayed)
+        rule = self.getFakeRule()
+        ruleColor = rule['color']
+        nbCardsToPut = rule['numberCardsToPut']
+        self.checkAllColors(self,item_dict,ruleColor)
+        self.checkNbCards(self,item_dict,nbCardsToPut)
+
+
+    def checkAllColors(self,item_dict,ruleColor):
+        for i in item_dict['cards']:
+            if (i['color'] == 'D') or (i['color'] == 'H'):
+                self.colorPlayed.append('rouge')
+            elif(i['color'] == 'C') or (i['color'] == 'S'):
+                self.colorPlayed.append('noir')
+                for j in self.colorPlayed:
+                    if j == ruleColor:
+                        self.colorsChecked += 1
+            else:
+                color = 'noir'
+
+    def checkNbCards(sefl,item_dict,nbCardsToPut):
+        if len(item_dict['cards']) == nbCardsToPut:
+            return True
+        else:
+            return False
+
+if __name__ == '__main__':
+    m = Main()
+    m.checkRules()
